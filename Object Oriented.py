c2ca4403794a7512333ef22e8038c5d5f0a880b3
Roberto Ortiz <103620280+peluzza11@users.noreply.github.com>
2023-02-24 18:02:27 -0800

    Add files via upload

---
+++ Object Oriented.py
@@ -0,0 +1,28 @@
+# First, he'll need a general Podracer class defined with max_speed, condition (perfect, trashed, repaired) and price attributes. 
+class Podracer:
+  def __init__(self, max_speed, condition, price):
+    self.max_speed = max_speed
+    self.condition = condition
+    self.price = price
+
+  # Define a repair() method that will update the condition of the podracer to "repaired".
+  def repair(self):
+    self.condition = "repaired"
+    
+# Define a new class, AnakinsPod that inherits the Podracer class, but also contains a special method called boost that will multiply max_speed by 2.
+class AnakinsPod(Podracer):
+  def __init__(self, max_speed, condition, price):
+    super.init(max_speed, condition, price)
+  
+  def boost(self):
+    self.max_speed *= 2
+    
+# Define another class that inherits Podracer and call this one SebulbasPod. 
+# This class should have a special method called flame_jet that will update the condition of another podracer to "trashed".
+class SebulbasPod(Podracer):
+  def __init__(self, max_speed, condition, price):
+    super.init(max_speed, condition, price)
+  
+  def flame_jet(self,other):
+    other.condition = "trashed"
+
---
+++ functional.py
@@ -0,0 +1,7 @@
+def flatten_and_sort(array):
+    arr = []
+    for item in array:
+        for i in item:
+            arr.append(i)
+    return sorted(arr)
+
